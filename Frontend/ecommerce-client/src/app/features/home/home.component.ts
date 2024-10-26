328a020ef133012d74ba5c42f7070472b697ffbf
Bryan042003 <bryancastrosolis732@gmail.com>
2024-10-25 21:25:11 -0600

    Header, sslogan and footer mosstt finished

--- Frontend/ecommerce-client/src/app/features/home/home.component.ts
+++ Frontend/ecommerce-client/src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { SloganComponent } from '../../components/slogan/slogan.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SloganComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
