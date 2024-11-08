d1dc97b62990733d74b27662d828d6c0f22586e1
RichLeonC <r29leonc@gmail.com>
2024-11-07 19:53:56 -0600

    User Model and Service

---
+++ Frontend/ecommerce-client/src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export interface User{
+    id: number;
+    email: string;
+    name: string;
+    last_name: string;
+    passw: string;
+    role: string;
+    photo: string;
+    phone: string;
+    id_address: number;
+
+}
---
+++ Frontend/ecommerce-client/src/app/services/Users.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+    private baseUrl = 'http://localhost:3000/api/users';
+    constructor(private http: HttpClient) { }
+    
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(this.baseUrl);
+      }
+    
+    getUser(id: string): Observable<User> {
+        return this.http.get<User>(`${this.baseUrl}/id/${id}`);
+      }
+
+    getUserByEmail(email: string): Observable<User> {
+        return this.http.get<User>(`${this.baseUrl}/email/${email}`);
+    }
+    
+    createUser(data: any): Observable<any> {
+        /*
+        data ={
+            email,
+            name,
+            last_name,
+            passw,
+            role,
+            photo,
+            phone,
+            province,
+            canton,
+            district,
+            postal_code,
+            specific_address
+        }
+        */
+        
+        return this.http.post(this.baseUrl, data);
+    }
+    
+    updateUser(id: string, data: User): Observable<User> {
+        /*
+        data = {
+            id,
+            email,
+            name,
+            last_name,
+            passw,
+            role,
+            photo,
+            phone,
+            id_address
+        }
+        */
+        
+        return this.http.put<User>(`${this.baseUrl}/${id}`, data);
+    }
+    
+    deleteUser(id: string): Observable<User> {
+        return this.http.delete<User>(`${this.baseUrl}/${id}`);
+    }
+}
+
+
+
