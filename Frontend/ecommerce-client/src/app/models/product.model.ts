32a0c0e63acd28a19e36db542a61823cc620984a
RichLeonC <r29leonc@gmail.com>
2024-11-08 19:48:41 -0600

    Progress Products Service

---
+++ Frontend/ecommerce-client/src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    brand:string;
+    photo:string;
+    technical_stuff:string;
+    stock:number;
+    n_sales:number;
+    id_offer:number;
+
+}
---
+++ Frontend/ecommerce-client/src/app/services/Product.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Product } from '../models/product.model';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService {
+    private baseUrl = `${environment.baseUrl}products`;
+    constructor(private http: HttpClient) { }
+    
+    getProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.baseUrl);
+      }
+    
+    getProduct(id: string): Observable<Product> {
+        return this.http.get<Product>(`${this.baseUrl}/id/${id}`);
+      }
+
+    getProductByName(name: string): Observable<Product> {
+        return this.http.get<Product>(`${this.baseUrl}/name/${name}`);
+    }
+    
+    createProduct(data: any): Observable<any> {
+        /*
+        data ={
+            name,
+            description,
+            price,
+            brand,
+            stock,
+            technical_stuff,
+            photo,
+        }
+        */
+        
+        return this.http.post(`${this.baseUrl}/create`, data);
+    }
+    
+    updateProduct(id: string, data: Product): Observable<Product> {
+
+        return this.http.put<Product>(`${this.baseUrl}/update/${id}`, data);
+    }
+    
+    deleteProduct(id: string): Observable<any> {
+        return this.http.delete(`${this.baseUrl}/id/${id}`);
+    }
+}
