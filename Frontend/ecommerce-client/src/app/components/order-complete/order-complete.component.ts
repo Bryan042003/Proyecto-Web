f06824043fafc73cdc95071c0f3558bf8b21a32f
Bryan042003 <bryancastrosolis732@gmail.com>
2024-11-08 18:00:10 -0600

    routes

--- Frontend/ecommerce-client/src/app/components/order-complete/order-complete.component.ts
+++ Frontend/ecommerce-client/src/app/components/order-complete/order-complete.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
 
 @Component({
   selector: 'app-order-complete',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './order-complete.component.html',
   styleUrl: './order-complete.component.scss'
 })
