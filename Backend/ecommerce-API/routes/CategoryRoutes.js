9151f6c17c93f09a3c754d42e35c842024319871
RichLeonC <r29leonc@gmail.com>
2024-11-11 16:27:27 -0600

    Categories Service

--- Backend/ecommerce-API/routes/CategoryRoutes.js
+++ Backend/ecommerce-API/routes/CategoryRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //Endpoints
 router.get('/', categoryController.getALLCategoriesWithSubcategories);
-router.get('/:category_id', categoryController.getCategoryWithSubcategories);
+router.get('/id/:category_id', categoryController.getCategoryWithSubcategories);
 router.get('/subcategories/:category_id', categoryController.getSubcategories);
 router.get('/parentCategory/:category_id', categoryController.getParentCategory);
 
