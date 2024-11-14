3d5944c711140b4a913e60b6d1d2fae1111ef361
Bryan042003 <bryancastrosolis732@gmail.com>
2024-11-13 23:49:44 -0600

    add review hecho

--- Frontend/ecommerce-client/src/app/components/price-view/price-view.component.ts
+++ Frontend/ecommerce-client/src/app/components/price-view/price-view.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { Offer } from '../../models/offer.model';
+import { OffersService } from '../../services/Offers.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-price-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './price-view.component.html',
   styleUrl: './price-view.component.scss'
 })
 export class PriceViewComponent {
+  @Input()
+  product!: Product;
+  descount: number = 0;
+
+  offer: Offer | undefined;
+
+  constructor(private offerservice: OffersService) { }
+
+  ngOnInit() {
+    if (this.product.id_offer !== null) {
+      this.offerservice.getOffer(this.product.id_offer.toString()).subscribe(offer => {
+        this.offer = offer;
+        this.descount = this.offer?.discount || 0;
+      });
+    }
+  }
 
 }
--- Frontend/ecommerce-client/src/app/components/star-rating/star-rating.component.ts
+++ Frontend/ecommerce-client/src/app/components/star-rating/star-rating.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,15 +7,47 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.scss'
+  styleUrl: './star-rating.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StarRatingComponent),
+      multi: true
+    }
+  ]
 })
 export class StarRatingComponent {
-  
-  rating = 0; // Valor de la calificación
+  @Input() rating: number = 0; // Calificación inicial
+  @Output() ratingChange = new EventEmitter<number>(); // Emite la calificación al componente padre
   stars = [1, 2, 3, 4, 5]; // Número de estrellas
 
-  rate(star: number): void {
-    this.rating = star; // Actualiza la calificación según la estrella seleccionada
+  // Función que maneja la actualización de la calificación y la emite al formulario
+  private onChange: (rating: number) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(rating: number): void {
+    if (rating !== undefined) {
+      this.rating = rating;
+    }
   }
 
+  registerOnChange(fn: (rating: number) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Aquí puedes manejar la lógica para deshabilitar el componente si es necesario
+  }
+
+
+  // Llamado cuando se selecciona una estrella
+  rate(star: number): void {
+    this.rating = star; // Actualiza la calificación
+    this.onChange(this.rating); // Notifica al formulario del cambio
+    this.ratingChange.emit(this.rating); // Emite al componente padre
+  }
 }
