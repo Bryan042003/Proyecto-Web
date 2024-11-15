28445ded8afc94583dff34dacb1d5f22110dc1f6
IssaAC00 <paola.alguera@estudiantec.cr>
2024-11-15 16:15:25 -0600

    login con token

---
+++ Frontend/ecommerce-client/src/app/features/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideBarComponent } from '../../components/side-bar/side-bar.component';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [SideBarComponent],
+  templateUrl: './admin-dashboard.component.html',
+  styles: ``
+})
+export class AdminDashboardComponent {
+
+}
